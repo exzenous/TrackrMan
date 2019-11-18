8cc25b11a146d69bf034d62a67ef1d5209a678cc
Nathachai <saimadma@gmail.com>
2019-11-19 00:36:55 +0700

    Create Track Pane/Main Window Pane

--- src/com/TrackrMan/AppRunner.java
+++ src/com/TrackrMan/AppRunner.java
@@ -1,7 +1,38 @@
 package com.TrackrMan;
 
-public class AppRunner {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class AppRunner extends Application {
+
+    Button button1,button2,button3;
+
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage mainWindow) throws Exception {
+        mainWindow.setTitle("TrackMan");
+
+        GridPane gridPane = new GridPane();
+        Scene firstScene = new Scene(gridPane);
+
+        button1 = new Button("First");
+        button2 = new Button("Second");
+        button3 = new Button("Third");
+        gridPane.add(button1,1,1);
+        gridPane.add(button2,1,2);
+        gridPane.add(button3,1,3);
+
+        mainWindow.setScene(firstScene);
+        mainWindow.sizeToScene();
+        mainWindow.show();
 
     }
 }
---
+++ src/com/TrackrMan/TrackPane.java
@@ -0,0 +1,31 @@
+package com.TrackrMan;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class TrackPane {
+
+    @FXML
+    private Label jumboTitle;
+
+    @FXML
+    private Label subTitle;
+
+    @FXML
+    private TextField inputCodeField;
+
+    @FXML
+    private ChoiceBox<?> vendorOption;
+
+    @FXML
+    private Button addBtn;
+
+    @FXML
+    private ListView<?> currentList;
+
+
+}
